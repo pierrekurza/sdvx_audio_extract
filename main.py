eff7b5a8407293022d4ed827ecd6801e6b4030cf
Pierre K <pierrekurzawski@gmail.com>
2024-02-04 22:02:14 +0100

    modified base code

--- main.py
+++ main.py
@@ -4,10 +4,29 @@ import pprint
 import requests
 import re
 import subprocess
-import sys
-import xml.etree.ElementTree as ET
+from PIL import Image
 
+# 0 : Music ID
 URL_GET_MUSIC_INFOS = "https://fairyjoke.net/api/games/sdvx/musics/{0}"
+
+# 0 : Music ID
+# 1 : Difficulty name
+URL_GET_MUSIC_COVER = "https://fairyjoke.net/api/games/sdvx/musics/{0}/{1}.png"
+
+URL_GET_DEFAULT_COVER = "https://fairyjoke.net/api/games/sdvx/assets/jacket/version.png"
+
+DIFF_LIST = ["NOVICE", "ADVANCED", "EXHAUST", "MAXIMUM", "INFINITE", "GRAVITY", "HEAVENLY", "VIVID", "EXCEED"]
+
+SDVX_NAME = "SOUND VOLTEX"
+
+GAME_VERSIONS = {
+    1: "SOUND VOLTEX BOOTH",
+    2: "SOUND VOLTEX II -infinite infection-",
+    3: "SOUND VOLTEX III GRAVITY WARS",
+    4: "SOUND VOLTEX IV HEAVENLY HAVEN",
+    5: "SOUND VOLTEX V VIVID WAVE",
+    6: "SOUND VOLTEX VI EXCEED GEAR"
+}
 
 
 # Get all the folder inside the provided file path
@@ -17,11 +36,6 @@ def get_folders_name(sound_path: str):
     for dir_name in list(sub_folders):
         sub_folders.extend(get_folders_name(dir_name))
     return sub_folders
-
-
-def get_music_infos(music_id: int):
-    response_api = requests.get(URL_GET_MUSIC_INFOS.format(music_id))
-    pprint.pprint((response_api.json()), sort_dicts=False)
 
 
 # Reads the current folder name
@@ -35,91 +49,75 @@ def get_folder_number(folder_name) -> int:
         else:
             return int(found_number_in_string.group()[0:5].lstrip('0'))
     else:
-        raise NameError("Nothing found")
+        raise NameError("Nothing found.")
 
 
-# Opens and reads the XML File provided
-# Returns the XML file as a file
-def read_music_xml(music_xml_file_path: str) -> ET.Element:
-    with open(music_xml_file_path, 'r', encoding="cp932") as file:
-        xml_string = file.read()
-        tree = ET.fromstring(xml_string)
-        return tree
-
-
-# Get music name, artist name from the XML File.
-# Returns music_name, artist_name, album_artist, album_name in this order.
-def get_music_infos_from_xml(song_id: int, music_xml_file_path: str):
-    music_name = ""
-    artist_name = ""
+def get_music_infos_from_api(music_id: int):
+    response_api = requests.get(URL_GET_MUSIC_INFOS.format(music_id))
+    if response_api.status_code == 404:
+        return
+    data = response_api.json()
+    music_name = data["title"]
+    artist_name = data["artist"]
     album_artist = "Various Artists"
-    album_name = "SOUND VOLTEX GST"
-    xml = read_music_xml(music_xml_file_path)
-    for music_info in xml.findall(".//music[@id='{0}']/info".format(song_id)):
-        if len(music_info.find('title_name').text) > 0:
-            music_name = music_info.find('title_name').text
-            artist_name = music_info.find('artist_name').text
-    return music_name, artist_name, album_artist, album_name
+    album_name = GAME_VERSIONS[data["version"]] + " GST"
+    max_diff = ""
+    simple_name = data["ascii"]
+    # Value can be : "EXHAUST"
+    if len(data["difficulties"]) == 3:
+        max_diff = data["difficulties"][2]['diff']
+    # Values can be : "MAXIMUM", "INFINITE", "GRAVITY", "HEAVENLY", "VIVID", "EXCEED"
+    elif len(data["difficulties"]) == 4:
+        max_diff = data["difficulties"][3]['diff']
+    return music_name, artist_name, album_artist, album_name, max_diff, simple_name
 
 
-# Retrieves the best resolution cover for a given folder name
-# Returns the path of the best resolution cover
-def get_music_cover(music_folder_path: str) -> str:
-    list_of_covers = glob.glob(music_folder_path + '\\*.png', recursive=True)
-    if len(list_of_covers) == 12:
-        other_pattern_available = "(_4_b.png$)"
-        first_try = browse_and_get_cover(list_of_covers, "(_5_b.png$)")
-        if first_try is None:
-            return browse_and_get_cover(list_of_covers, other_pattern_available)
-        return first_try
-    # 2 types of covers available
-    elif len(list_of_covers) == 6:
-        other_pattern_available = "(_4_b.png$)"
-        first_try = browse_and_get_cover(list_of_covers, "(_5_b.png$)")
-        # No cover found for the first try
-        if first_try is None:
-            return browse_and_get_cover(list_of_covers, other_pattern_available)
-        return first_try
-    # 1 cover available
-    elif len(list_of_covers) == 3:
-        other_pattern_available = "(_4_b.png$)"
-        first_try = browse_and_get_cover(list_of_covers, "(_1_b.png$)")
-        if first_try is None:
-            return browse_and_get_cover(list_of_covers, other_pattern_available)
-        return first_try
-    # 3 covers available
-    elif len(list_of_covers) == 9:
-        return browse_and_get_cover(list_of_covers, "(_3_b.png$)")
-
-
-def browse_and_get_cover(list_of_covers: list[str], search_pattern: str):
-    for cover in list_of_covers:
-        if re.search(search_pattern, cover):
-            return cover
+def get_music_cover_from_api(music_id: int, diff_name: str, extract_folder: str) -> str:
+    if diff_name.upper() in DIFF_LIST:
+        image = ""
+        response_api = requests.get(URL_GET_MUSIC_COVER.format(music_id, diff_name), stream=True)
+        if response_api.status_code == 404:
+            default_sdvx_cover_response = requests.get(URL_GET_DEFAULT_COVER, stream=True)
+            image = Image.open(default_sdvx_cover_response.raw)
+        else:
+            image = Image.open(response_api.raw)
+        if os.path.exists(extract_folder):
+            covers_path = extract_folder + "\\covers"
+            os.mkdir(covers_path)
+            os.chdir(covers_path)
+            final_cover_path = f"{covers_path}" + "\\{0}.png".format(str(music_id))
+            if not os.path.isfile(final_cover_path):
+                image.save(f"{final_cover_path}")
+            return os.getcwd() + "\\{0}.png".format(str(music_id))
+    else:
+        raise NameError("Cover not found.")
 
 
 def convert_audio_and_move_file(folder_path: str, folder_number: int, output_path: str, music_name: str,
-                                artist_name: str, album_artist: str, album_name: str):
+                                artist_name: str, album_artist: str, album_name: str, ascii_name: str, diff_name: str):
     list_of_files = glob.glob(folder_path + '\\*.s3v', recursive=True)
     if len(list_of_files) == 0:
         return
     if not music_name.strip():
         return
     music_path: str = ""
-    cover_path = get_music_cover(folder_path)
+
+    cover_path = get_music_cover_from_api(folder_number, diff_name, output_path)
     for music in list_of_files:
         if not re.search("(_pre.s3v$)", music):
             music_path = music
     # Launch extract
-    output_path_final = output_path + "\\" + str(folder_number) + ' - ' + music_name + ".mp3"
-    command_line = '''C:\\TOOLS\\ffmpeg.exe -i "%s" -i "%s" -map 0:0 -map 1:0 -c copy -acodec libmp3lame -id3v2_version 3 -metadata:s:v title="Album cover" -metadata:s:v comment="Cover (front)" -metadata title="%s" -metadata artist="%s" -metadata album_artist="%s" -metadata album="%s" -q:a 0 "%s"'''
+    output_path_final = os.path.join(output_path, album_name, str(folder_number), ascii_name, ".mp3")
+    command_line = '''C:\\TOOLS\\ffmpeg.exe -i "%s" -i "%s" -map 0:0 -map 1:0 -ab 320k -c copy -acodec libmp3lame 
+    -id3v2_version 3 -metadata:s:v title="Album cover" -metadata:s:v comment="Cover (front)" -metadata title="%s" 
+    -metadata artist="%s" -metadata album_artist="%s" -metadata album="%s" -q:a 0 "%s"'''
     process = subprocess.Popen(command_line % (
         music_path,
         cover_path,
         music_name,
-        artist_name.replace("\"", "\\\""),
+        artist_name,
         album_artist,
-        album_name,
+        album_name + " GST",
         output_path_final
     ), shell=True)
     if process:
@@ -128,25 +126,36 @@ def convert_audio_and_move_file(folder_path: str, folder_number: int, output_pat
         return False
 
 
+def introduction_cli():
+    print("SDVX Songs Extraction Tool")
+    while True:
+        game_folder = input("Please provide the SDVX game path > ")
+        if os.path.exists(game_folder) and "soundvoltex.dll" in os.listdir(game_folder + "\\modules"):
+            print("Continue")
+            break
+        else:
+            print("soundvoltex.dll is not present...")
+    while True:
+        extract_folder = input("Please provide the path where musics have to be extracted (the folder will be created "
+                               "if it doesn't exist) > ")
+        break
+    return game_folder, extract_folder
+
+
 def main():
-    get_music_infos(1773)
-    # args = sys.argv[1:]
-    # if len(args) == 4 and args[0] == '-audiopath' and args[2] == '-musicxmlpath':
-    #     # specify the path for the output folder
-    #     output_folder = "G:\\DEV_SDVX"
-    #     # rename SDVX_SONG to whatever you like
-    #     output_folder_name = "\\" + "SDVX_SONGS_2"
-    #     final_output_folder_name = output_folder + output_folder_name
-    #     if not os.path.exists(final_output_folder_name):
-    #         os.makedirs(final_output_folder_name)
-    #     audio_path = args[1]
-    #     xml_path = args[3]
-    #     folders = get_folders_name(audio_path)
-    #     for folder in folders:
-    #         folder_number = get_folder_number(folder)
-    #         music_name, artist_name, album_artist, album_name = get_music_infos_from_xml(folder_number, xml_path)
-    #         convert_audio_and_move_file(folder, folder_number, final_output_folder_name, music_name, artist_name,
-    #                                     album_artist, album_name)
+    game_folder, extract_folder = introduction_cli()
+    covers_dir = glob.glob(extract_folder + "\\covers" + "\\*")
+    for i in covers_dir:
+        os.remove(i)
+    if os.path.isdir(os.path.join(extract_folder, "covers")):
+        os.rmdir(extract_folder + "\\covers")
+    music_folder = os.path.join(game_folder, "data", "music")
+    folders = get_folders_name(music_folder)
+    for folder in folders:
+        folder_number = get_folder_number(folder)
+        music_name, artist_name, album_artist, album_name, max_diff, simple_name = get_music_infos_from_api(folder_number)
+        convert_audio_and_move_file(music_name, folder_number, extract_folder, music_name, artist_name, album_artist,
+                                    album_name, simple_name, max_diff)
 
 
 if __name__ == '__main__':
